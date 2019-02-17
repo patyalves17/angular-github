33cb4290fa1335560e8ef288325428129dce7733
patyalves17 <patriciaalves10@gmail.com>
2019-02-17 18:29:39 -0300

    git localhost

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,6 @@
-export const CLIENT_ID = '6e3eb5c724196e917235';
-export const CLIENT_SECRET = '4f5e1b847bd63dea9df0a9c7e4cc3c93b40b3bc1';
+// export const CLIENT_ID = '6e3eb5c724196e917235';
+// export const CLIENT_SECRET = '4f5e1b847bd63dea9df0a9c7e4cc3c93b40b3bc1';
+
+
+export const CLIENT_ID = '7f8d5de0cd6f5d33bf26';
+export const CLIENT_SECRET = 'b8ef37d3f303122421513a9fe30797167f4ec8dd';
